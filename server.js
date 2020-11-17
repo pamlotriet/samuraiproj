3da172c6fedae0cc3b08d6ea012521017c2153bd
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 15:52:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+
+const userRoutes = require('./userRoutes');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
