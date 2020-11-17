349b2cf05af199042abd86d0cfc35938ce9bd2ba
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 16:09:52 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true})); 
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+//connect to mongo 
+const MONGO_URI ='mongodb+srv://Pamela:'+process.env.ATLAS_PASS+'@samusers.c98z3.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+mongoose.connect(process.env.MONGO_URI || MONGO_URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!');
+});
+
+
+//creating user schema
+
+const userSchema = mongoose.Schema({
+    name:{type: String},
+    surname: {type: String},
+    email: {type: String, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ },
+    password:{type: String},
+});
+
+//register new user
+app.post('/register.html', (req,res,next)=> {
+    userSchema.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length){
+            return res.status(409).json({
+                message: 'Email already exists' 
+            });
+        }
+        else{
+                    const user = new userSchema({
+                        name: req.body.name,
+                        surname: req.body.surname,
+                        email: req.body.email,
+                        password: req.body.password,
+                    });
+        
+                    user.save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(201).json({
+                                message: 'User created'
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            });
+                        });
+                }
+            });
+});
+
+
+app.listen(PORT);
