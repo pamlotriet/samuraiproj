d7aec1ccd8696ed41aee54451f3c2226735829db
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 17:23:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const path = require('path'
 const app = express();
 const bodyParser = require('body-parser');
-const userRoutes = require(path.join(__dirname,'/api/userRoutes.js'));
+const userRoutes = require('samuraiproj/api/userRoutes.js');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
