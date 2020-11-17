2459ba69499e1718f81abe674420c000aa10cf6c
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 13:27:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.use(express.static(__dirname));
 
 app.get('/',(req,res)=>{
