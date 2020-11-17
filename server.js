5b5b15df96a827ed3efe172363ea8ea87f9fb1ff
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 17:46:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+import User from './schemas/userSchema';
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -26,6 +26,46 @@ mongoose.connect(process.env.MONGO_URI || MONGO_URI, {
 
 mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected!');
+});
+
+
+
+
+//register new user
+
+router.post('/register.html', (req,res,next)=> {
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length){
+            return res.status(409).json({
+                message: 'Email already exists' 
+            });
+        }
+        else{
+            
+                    const user = new User({
+                        name: req.body.name,
+                        surname: req.body.surname,
+                        email: req.body.email,
+                        password:  req.body.password
+                    });
+        
+                    user.save()
+                        .then(result => {
+                            console.log(result);
+                            res.status(201).json({
+                                message: 'User created'
+                            });
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            });
+                        });
+                }
+            });
 });
 
 
