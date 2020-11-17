652795fa91d4a34c447ba3c0cf5df883870c5b8a
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 13:19:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 });
 
-//connect to mongo //
-
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-});
 
 
 app.listen(PORT);
