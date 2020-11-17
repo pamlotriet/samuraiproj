28ea661f928674dd77c076acbfddf4cbde6856fb
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 16:29:00 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
+const userRoutes = require('./api/auserRoutes');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true})); 
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+//connect to mongo 
+const MONGO_URI ='mongodb+srv://Pamela:'+process.env.ATLAS_PASS+'@samusers.c98z3.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+mongoose.connect(process.env.MONGO_URI || MONGO_URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!');
+});
+
+
+app.listen(PORT);
