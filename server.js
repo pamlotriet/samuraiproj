c01646843696ac500572cac2b6f7deb4ed7f15b1
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 18:03:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,45 +28,49 @@ mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected!');
 });
 
-
-
-
-//register new user
-
-app.post('/register.html', (req,res,next)=> {
-    User.find({email: req.body.email})
-    .exec()
-    .then(user => {
-        if(user.length){
-            return res.status(409).json({
-                message: 'Email already exists' 
-            });
-        }
-        else{
-            
-                    const user = new User({
-                        name: req.body.name,
-                        surname: req.body.surname,
-                        email: req.body.email,
-                        password:  req.body.password
-                    });
-        
-                    user.save()
-                        .then(result => {
-                            console.log(result);
-                            res.status(201).json({
-                                message: 'User created'
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.status(500).json({
-                                error: err
-                            });
-                        });
-                }
-            });
+//creating user schema
+const userSchema = mongoose.Schema({
+    name:{type: String},
+    surname: {type: String},
+    email: {type: String, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ },
+    password:{type: String},
 });
+
+
+//registering new user
+app.post('/register.html', (req,res,next)=> {
+        User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if(user.length >1){
+                return res.status(409).json({
+                    message: 'Email already exists' 
+                });
+            }
+            else{
+                        const user = new userSchema({
+                            name: req.body.name,
+                            surname: req.body.surname,
+                            email: req.body.email,
+                            password: req.body.password
+                        });
+            
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: 'User created'
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+    });
 
 
 app.listen(PORT);
