16bab3af7dc2d07728222aadc1b3f4acb85e7a13
pamlotriet <54804761+pamlotriet@users.noreply.github.com>
2020-11-17 12:54:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html');
 });
 
-//connect to mongo 
+//connect to mongo //
 const MONGO_URI ='mongodb+srv://Pamela:'+process.env.ATLAS_PASS+'@samusers.c98z3.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 mongoose.connect(process.env.MONGO_URI || MONGO_URI, {
